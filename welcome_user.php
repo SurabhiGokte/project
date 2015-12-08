77ffc2c9dc898d177fba4d07461164410f32a5b6
panshulk <panshul.khurana@srijan.in>
2015-12-08 13:39:43 +0530

    Altered Welcome_user file for admin login

--- welcome_user.php
+++ welcome_user.php
@@ -4,28 +4,35 @@
 	
 	$email = $_POST['email'];
 	$password1 = $_POST['password'];
-
-	$dbconn = mysqli_connect($hostname,$username,$password);
-	if(!$dbconn){
-		die("Could not connect to the database : ".mysql_error());
+	if($email=='admin@tbs.in' && $password1=='admin')
+	{
+		header('Location: /project/admin/all_movies.php');
 	}
-	else{
-		$dbselect = mysqli_select_db($dbconn,$dbname);
-			if(!$dbselect){
-				die("Could not select the database : ".mysql_error());
-			}
-			else{
-				$query = "SELECT * FROM user WHERE user_email='$email' and user_password='$password1'";
-				if($query_run = mysqli_query($dbconn,$query)){
-					while($row = mysqli_fetch_array($query_run)){
-						$name = $row['user_fname'];
-						$user_id = $row['user_id'];
-						$_SESSION['user_id'] = $user_id;
-						echo("<center><p>Welcome $name</p>");
+	else
+
+	{	
+		$dbconn = mysqli_connect($hostname,$username,$password);
+		if(!$dbconn){
+			die("Could not connect to the database : ".mysql_error());
+		}
+		else{
+			$dbselect = mysqli_select_db($dbconn,$dbname);
+				if(!$dbselect){
+					die("Could not select the database : ".mysql_error());
+				}
+				else{
+					$query = "SELECT * FROM user WHERE user_email='$email' and user_password='$password1'";
+					if($query_run = mysqli_query($dbconn,$query)){
+						while($row = mysqli_fetch_array($query_run)){
+							$name = $row['user_fname'];
+							$user_id = $row['user_id'];
+							$_SESSION['user_id'] = $user_id;
+							echo("<center><p>Welcome $name</p>");
+						}
 					}
 				}
-			}
-			mysqli_close($dbconn);
+				mysqli_close($dbconn);
+		}
 	}
 ?>
 <html>
